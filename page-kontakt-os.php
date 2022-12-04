c6ec6b93c5406380b4abdac015ed0ea261e628cc
Regitze-Adrian <regi5541@easv365.dk>
2022-12-05 00:57:54 +0100

    acf
    acf

--- page-kontakt-os.php
+++ page-kontakt-os.php
@@ -3,7 +3,8 @@
 <?php while(have_posts()): the_post() ?>
 
 <div class="kontakt">
-    <p> <?php the_field("kontakt_titel") ?> 
+    <p> <?php the_field("kontakt_titel") ?>
+    <p> <?php the_field("kontakt_info") ?> 
     <p> <?php the_field("kontakt_navn") ?>
     <p> <?php the_field("kontakt_vej") ?>
     <p> <?php the_field("kontakt_by") ?>
